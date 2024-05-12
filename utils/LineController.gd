65b615433224710d497d2d7ab335f0f99725ea5d
Francois <fnlombard@gmail.com>
2024-05-12 15:42:07 +0200

    Improve line algorithm.

--- utils/LineController.gd
+++ utils/LineController.gd
@@ -8,31 +8,33 @@ func _init(parent: Node2D) -> void:
     self._parent = parent
 
 func tick() -> void:
-    """
-    Go through each of the lines and update the edge positions.
-    """
     for area in self._line_maps.keys():
-        for child in area.get_children():
-            if not child is CollisionShape2D:
-                continue
+        process_area(area)
 
-            var shape = child.shape
-            match shape.get_class():
-                "RectangleShape2D":
-                    _process_rect(area, shape)
-                "CircleShape2D":
-                    print("CircleShape2D", shape.radius)
-                "CapsuleShape2D":
-                    print("CapsuleShape2D", shape.radius, shape.height)
-                "ConvexPolygonShape2D", "ConcavePolygonShape2D":
-                    print("ConvexPolygonShape2D, ConcavePolygonShape2D", shape.points)
-                _:
-                    print("Other Shape Type Detected:", child.shape)
-            break
+func process_area(area: Area2D) -> void:
+    for child in area.get_children():
+        if not child is CollisionShape2D:
+            continue
+
+        var shape = child.shape
+        match shape.get_class():
+            "RectangleShape2D":
+                _process_rect(area, shape)
+            "CircleShape2D":
+                print("CircleShape2D", shape.radius)
+            "CapsuleShape2D":
+                print("CapsuleShape2D", shape.radius, shape.height)
+            "ConvexPolygonShape2D", "ConcavePolygonShape2D":
+                print("ConvexPolygonShape2D, ConcavePolygonShape2D", shape.points)
+            _:
+                print("Other Shape Type Detected:", child.shape)
+        return
 
 func remove_area_lines(area: Area2D) -> void:
     for line in self._line_maps[area]:
         self._parent.remove_child(line)
+
+    self._line_maps[area] = []
 
 func remove_area(area: Area2D) -> void:
     remove_area_lines(area)
@@ -40,27 +42,36 @@ func remove_area(area: Area2D) -> void:
 
 func add_area(area: Area2D) -> void:
     self._line_maps[area] = []
+    process_area(area)
 
 func _process_rect(rect_area: Area2D, shape: RectangleShape2D) -> void:
-    var player_center = PlayerVariables.position_get()
     var player_rotation = PlayerVariables.rotation_get()
 
     var shape_center = rect_area.global_position
     var extents = shape.extents
 
-    var corners = [
+    var points = [
         shape_center + extents.rotated(player_rotation),
         shape_center + Vector2(extents.x, -extents.y).rotated(player_rotation),
         shape_center - extents.rotated(player_rotation),
         shape_center + Vector2( - extents.x, extents.y).rotated(player_rotation)
     ]
 
-    remove_area_lines(rect_area)
-    self._line_maps[rect_area] = []
-    var lines = self._line_maps[rect_area]
+    _update_lines(self._line_maps[rect_area], points)
 
-    for corner in corners:
-        var line = _create_line(player_center, corner)
+func _update_lines(lines: Array, points: Array) -> void:
+    if not lines:
+        _add_lines(lines, points)
+
+    var player_center = PlayerVariables.position_get()
+    for index in range(len(lines)):
+        lines[index].set_point_position(0, player_center)
+        lines[index].set_point_position(1, points[index])
+
+func _add_lines(lines: Array, points: Array) -> void:
+    var player_center = PlayerVariables.position_get()
+    for point in points:
+        var line = _create_line(player_center, point)
 
         lines.append(line)
         self._parent.add_child(line)
