a9e648a5725fb113e4bcde64ef2e8df7ca7810f7
Francois <fnlombard@gmail.com>
2024-05-12 15:00:15 +0200

    Follow player.

--- utils/LineController.gd
+++ utils/LineController.gd
@@ -11,36 +11,35 @@ func tick() -> void:
     """
     Go through each of the lines and update the edge positions.
     """
-    pass
+    for area in self._line_maps.keys():
+        for child in area.get_children():
+            if not child is CollisionShape2D:
+                continue
 
-func remove_area(area: Area2D) -> void:
+            var shape = child.shape
+            match shape.get_class():
+                "RectangleShape2D":
+                    _process_rect(area, shape)
+                "CircleShape2D":
+                    print("CircleShape2D", shape.radius)
+                "CapsuleShape2D":
+                    print("CapsuleShape2D", shape.radius, shape.height)
+                "ConvexPolygonShape2D", "ConcavePolygonShape2D":
+                    print("ConvexPolygonShape2D, ConcavePolygonShape2D", shape.points)
+                _:
+                    print("Other Shape Type Detected:", child.shape)
+            break
+
+func remove_area_lines(area: Area2D) -> void:
     for line in self._line_maps[area]:
         self._parent.remove_child(line)
+
+func remove_area(area: Area2D) -> void:
+    remove_area_lines(area)
     self._line_maps.erase(area)
 
 func add_area(area: Area2D) -> void:
-    """
-    Process area and calculate lines to the edges.
-    """
-    for child in area.get_children():
-        if not child is CollisionShape2D:
-            continue
-
-        var shape = child.shape
-        match shape.get_class():
-            "RectangleShape2D":
-                _process_rect(area, shape)
-            "CircleShape2D":
-                print("CircleShape2D", shape.radius)
-            "CapsuleShape2D":
-                print("CapsuleShape2D", shape.radius, shape.height)
-            "ConvexPolygonShape2D", "ConcavePolygonShape2D":
-                print("ConvexPolygonShape2D, ConcavePolygonShape2D", shape.points)
-            _:
-                print("Other Shape Type Detected:", child.shape)
-        return
-
-    print("No CollisionShape2D found in the area.")
+    self._line_maps[area] = []
 
 func _process_rect(rect_area: Area2D, shape: RectangleShape2D) -> void:
     var player_center = PlayerVariables.position_get()
@@ -56,6 +55,7 @@ func _process_rect(rect_area: Area2D, shape: RectangleShape2D) -> void:
         shape_center + Vector2( - extents.x, extents.y).rotated(player_rotation)
     ]
 
+    remove_area_lines(rect_area)
     self._line_maps[rect_area] = []
     var lines = self._line_maps[rect_area]
 
